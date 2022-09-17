e6bbbb3bac233f8552a25398ed6806bb0eb4d959
boggs <6130147+b-ggs@users.noreply.github.com>
2022-09-17 22:10:19 +0800

    Add Sentry test URL

--- django3_template/urls.py
+++ django3_template/urls.py
@@ -1,21 +1,16 @@
-"""django3_template URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+if hasattr(settings, "SENTRY_TEST_URL_ENABLED") and settings.SENTRY_TEST_URL_ENABLED:
+
+    def trigger_error(request):
+        return 1 / 0
+
+    urlpatterns += [
+        path("_trigger-error/", trigger_error),
+    ]
