ca31e8541c46565469e5c995d1ca915bd8851b6f
boggs <6130147+b-ggs@users.noreply.github.com>
2022-10-27 22:32:06 +0800

    Add python version and django version to homepage

--- django3_template/home/views.py
+++ django3_template/home/views.py
@@ -1,5 +1,16 @@
+import platform
+
+from django import get_version
 from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
     template_name = "home/index.html"
+
+    def get_context_data(self, **kwargs: dict) -> dict:
+        context = super().get_context_data(**kwargs)
+        context |= {
+            "python_version": ".".join(platform.python_version_tuple()),
+            "django_version": get_version(),
+        }
+        return context
