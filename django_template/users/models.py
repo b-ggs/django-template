6b4cc1780106264aa97f459c37aca99a7d602682
boggs <6130147+b-ggs@users.noreply.github.com>
2023-02-11 07:41:28 +0800

    Revert "Test the action itself"
    This reverts commit ea41a51cae2b00d661a589438184b2246dffd7ed.

--- django_template/users/models.py
+++ django_template/users/models.py
@@ -1,10 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 class User(AbstractUser):
     pass
-
-
-class TestModel(models.Model):
-    test = models.CharField()
