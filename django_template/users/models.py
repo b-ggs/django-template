ea41a51cae2b00d661a589438184b2246dffd7ed
boggs <6130147+b-ggs@users.noreply.github.com>
2023-02-11 07:37:47 +0800

    Test the action itself

--- django_template/users/models.py
+++ django_template/users/models.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class TestModel(models.Model):
+    test = models.CharField()
