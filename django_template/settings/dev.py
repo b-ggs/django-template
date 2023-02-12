684cc81048561e52fd13600a15589edda31050b6
boggs <6130147+b-ggs@users.noreply.github.com>
2023-02-12 10:24:37 +0800

    Remove isort, flake8, replace with ruff

--- django_template/settings/dev.py
+++ django_template/settings/dev.py
@@ -6,7 +6,7 @@ DJANGO_ENV = "development"
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-nvg5arlsvczsdk5pzu-=f2qpst%ze8#jyuhfmldp7--j#ao5)j"  # pragma: allowlist secret  # nosec
+SECRET_KEY = "django-insecure-nvg5arlsvczsdk5pzu-=f2qpst%ze8#jyuhfmldp7--j#ao5)j"  # pragma: allowlist secret  # nosec  # noqa: E501
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
