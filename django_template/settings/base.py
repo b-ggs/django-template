4ef28968f18c7d7eed631dac0c69244a7a4a7f7d
boggs 🌊 <6130147+b-ggs@users.noreply.github.com>
2025-07-27 21:43:37 +0800

    Set conn_max_age and conn_health_checks

--- django_template/settings/base.py
+++ django_template/settings/base.py
@@ -79,7 +79,11 @@ WSGI_APPLICATION = "django_template.wsgi.application"
 
 if "DATABASE_URL" in os.environ:
     DATABASES = {
-        "default": dj_database_url.parse(os.environ["DATABASE_URL"]),
+        "default": dj_database_url.parse(
+            os.environ["DATABASE_URL"],
+            conn_max_age=600,
+            conn_health_checks=True,
+        ),
     }
 
 
